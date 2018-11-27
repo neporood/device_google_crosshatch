34f4c31a6d53c1d3885c49ae9b58ea30c9f2e74b
Ethan Chen <intervigil@gmail.com>
2018-11-26 23:36:11 -0800

    b1c1: Initial Scorpion bringup
    Change-Id: Ic4b0b00cf511112839a8d25e095cd0a849eca26b

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -20,8 +20,12 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_crosshatch_hwasan.mk \
     $(LOCAL_DIR)/aosp_blueline_hwasan.mk \
     $(LOCAL_DIR)/vf/aosp_crosshatch_vf.mk \
+    $(LOCAL_DIR)/scorpion_blueline.mk \
+    $(LOCAL_DIR)/scorpion_crosshatch.mk
 
 COMMON_LUNCH_CHOICES := \
     aosp_crosshatch-userdebug \
     aosp_blueline-userdebug \
     aosp_crosshatch_vf-userdebug \
+    scorpion_blueline-userdebug \
+    scorpion_crosshatch-userdebug
